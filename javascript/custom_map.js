ef33a26b438d9dfbd46752c36fe0294964591826
weidenfreak <meike.wiemann@gmx.de>
2014-12-31 12:42:12 +0100

    move legend to separate file

--- javascript/custom_map.js
+++ javascript/custom_map.js
@@ -7,9 +7,6 @@ var widthScale = 900,
 var minYear = 1990,
     maxYear = 2012;
 
-var legendRectSize = 20;                                  // NEW
-var legendSpacing = 4;                                    // NEW
-
 //set min and max year for slider dynamically
 $("#yearRange")
   .attr({min: minYear})
@@ -124,32 +121,6 @@ var color = d3.scale.quantize()
       })
   })
 
-  var legend = svg.selectAll('.legend')
-    .data(color.range())
-    .enter()
-    .append('g')
-    .attr('class', 'legend')
-    .attr('transform', function(d, i) {
-      var height = legendRectSize + legendSpacing;
-      var horz = 2 * legendRectSize;
-      var vert = i * height + 380;
-      return 'translate(' + horz + ',' + vert + ')';
-    });
-
-  legend.append('rect')
-    .attr('width', legendRectSize)
-    .attr('height', legendRectSize)
-    .style("fill", function(d) {
-      return d;
-    })
-
-  legend.append('text')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function(d) {
-      var range = color.invertExtent(d)
-      return range[0] + "% - " + range[1] + "%";
-    });
 
   function tooltip(d, year) {
     return d.properties.NAME + ": " + readYearValue(d, year) + "%" + " (" + year + ")";
